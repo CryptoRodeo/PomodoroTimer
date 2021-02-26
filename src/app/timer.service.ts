7b8674451342a266f5aa81e28edaa8592733d7c0
Kilgore Trout <b.r.tech95@gmail.com>
2021-02-26 07:27:37 -0500

    add start and stop time functionality

--- src/app/timer.service.ts
+++ src/app/timer.service.ts
@@ -4,26 +4,42 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TimerService {
-  private timeInterval: number = 1800; //Default time is 30 Minutes
+  pomodoroTime = 1800; //Default time is 30 minutes
+  intervalID = 0;
 
-  ngOnInit 
 
-  formatTime(timeInMs: number): string {
-    let seconds = (timeInMs / 1000).toFixed(1);
-    let minutes = (timeInMs / (1000 * 60)).toFixed(1);
-    return `${minutes} : ${seconds}`;
-  }
   constructor() { 
   }
 
-  countDown() 
+  formatTime(timeInS: number): string {
+    let time = new Date(timeInS * 1000);
+    /**
+     * Get minutes and seconds, add padding.
+     */
+    let minutes = time.getUTCMinutes().toString().padStart(2,'0');
+    let seconds = time.getSeconds().toString().padStart(2,'0');
+    return `${minutes} : ${seconds}`;
+  }
+
+  startTimer()
   {
-    this.timeInterval--;
-    setInterval(this.countDown, 1000);
+    // Call this.countDown, bind the call to this service using arrow operator.
+    this.intervalID = window.setInterval(() => this.countDown(), 1000);
+  }
+
+  getTime(): string
+  {
+    return this.formatTime(this.pomodoroTime);
+  }
+
+  countDown(): void 
+  {
+    this.pomodoroTime--;
   }
 
   stopTimer()
   {
-    clearInterval(this.countDown);
+    clearInterval(this.intervalID);
   }
+
 }
