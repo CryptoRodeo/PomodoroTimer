a589f86b251bf0939cd79c451beddb65ae468dca
Kilgore Trout <b.r.tech95@gmail.com>
2021-03-09 06:43:32 -0500

    Mostly working modal, still need to add content and stylings

--- src/app/settings-page/settings-page.component.ts
+++ src/app/settings-page/settings-page.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, Input, OnInit, ViewEncapsulation, OnDestroy } from '@angular/core';
+import { ModalService } from '../modal.service';
 @Component({
   selector: 'app-settings-page',
   templateUrl: './settings-page.component.html',
-  styleUrls: ['./settings-page.component.css']
+  styleUrls: ['./settings-page.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class SettingsPageComponent implements OnInit {
-
-  constructor() { }
+export class SettingsPageComponent implements OnInit, OnDestroy {
+  @Input() id: string;
+  private element: any;
+  constructor(private modalService: ModalService, private el: ElementRef) {
+    this.element = el.nativeElement;
+  }
 
   ngOnInit(): void {
+    if (!this.id) {
+      console.error("Component using the modal service must have an ID");
+      return;
+    }
+
+
+    //Move element to the bottom of the page (before </body>) so it can
+    // be displayed above everything else.
+
+    document.body.appendChild(this.element);
+
+    this.element.addEventListener('click', el => {
+      if (el.target.className === 'app-settings-page') {
+        this.close();
+      }
+    });
+
+    // add this modal instance to the modal service so its accessible from controllers
+    this.modalService.add(this);
+  }
+
+  ngOnDestroy(): void {
+    this.modalService.remove(this.id);
+    this.element.remove();
+  }
+
+  open(): void {
+    this.element.style.display == 'block';
+    document.body.classList.add('settings-page-open');
+  }
+
+  close(): void {
+    this.element.style.display == 'none';
+    document.body.classList.remove('settings-page-open');
   }
 
 }
