83b4353ed4636fae44a4a5884ef6f056cde490ee
Kilgore Trout <b.r.tech95@gmail.com>
2021-03-30 07:40:31 -0400

    add form group and controls to settings component

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { TimerService } from '../timer.service';
 import { NotificationService } from '../notification.service';
 import { PomodoroTimePeriods } from '../pomodoro-time-periods';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
@@ -9,7 +10,24 @@ import { PomodoroTimePeriods } from '../pomodoro-time-periods';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-
+  settingsForm = new FormGroup({
+    alertPreferences: new FormGroup({
+      soundOptions: new FormControl() 
+    }),
+    volumePreferences: new FormGroup({
+      volumeOptions: new FormControl({})
+    }),
+    notificationPreferences: new FormGroup({
+      timerIndication: new FormControl(true),
+      browserNotification: new FormControl(true),
+      autoStartTimer: new FormControl(true)
+    }),
+    timerPreferences: new FormGroup({
+      pomodoroSetting: new FormControl(this.getTimePeriods().pomodoro),
+      shortBreakSetting: new FormControl(this.getTimePeriods().shortBreak),
+      longBreakSetting: new FormControl(this.getTimePeriods().longBreak)
+    })
+  });
   constructor(private timerService: TimerService, private notificationService: NotificationService) { }
 
   ngOnInit(): void {
@@ -17,6 +35,7 @@ export class SettingsComponent implements OnInit {
 
   logTimePeriods(): void {
     console.log(this.timerService.getTimePeriods());
+    console.log(this.settingsForm);
   }
 
   getTimePeriods(): PomodoroTimePeriods {
