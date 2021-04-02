5141bd6272e8400dfeb15bb42d2e43d085a444aa
Kilgore Trout <b.r.tech95@gmail.com>
2021-04-02 06:54:40 -0400

    move values to functions, delegate setting changes to their respective functions

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -10,23 +10,18 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
-  // Audio alter files
-  audioFiles: Array<String> = Object.getOwnPropertyNames(this.notificationService.getAudioFiles());
-  browserNotificationPermission: Boolean = this.notificationService.getPermissionGranted();
-  browserTabNotificationAllowed: Boolean = this.timerService.browserTabIndicationAllowed();
-  autoStartTimers: Boolean = this.timerService.autoStartTimers();
 
   settingsForm = new FormGroup({
     alertPreferences: new FormGroup({
-      soundOptions: new FormControl(this.audioFiles)
+      soundOptions: new FormControl(this.getAudioFiles())
     }),
     volumePreferences: new FormGroup({
       volumeOptions: new FormControl({})
     }),
     notificationPreferences: new FormGroup({
-      timerIndication: new FormControl(this.browserTabNotificationAllowed),
-      browserNotification: new FormControl(this.browserNotificationPermission),
-      autoStartTimer: new FormControl(this.autoStartTimers)
+      timerIndication: new FormControl(this.getBrowserTabPermission()),
+      browserNotification: new FormControl(this.getBrowserNotificationPermission()),
+      autoStartTimer: new FormControl(this.autoStartTimer())
     }),
     timerPreferences: new FormGroup({
       pomodoroSetting: new FormControl(this.getTimePeriods().pomodoro),
@@ -39,12 +34,60 @@ export class SettingsComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  logTimePeriods(): void {
-    console.log(this.timerService.getTimePeriods());
-    console.log(this.settingsForm);
-  }
-
   getTimePeriods(): PomodoroTimePeriods {
     return this.timerService.getTimePeriods();
+  }
+
+  getAudioFiles(): Array<String> {
+    return Object.getOwnPropertyNames(this.notificationService.getAudioFiles());
+  }
+
+  getBrowserNotificationPermission(): Boolean {
+    return this.notificationService.getPermissionGranted();
+  }
+
+  getBrowserTabPermission(): Boolean {
+    return this.timerService.browserTabIndicationAllowed();
+  }
+
+  autoStartTimer(): Boolean {
+    return this.timerService.autoStartTimers();
+  }
+
+  applySettings(): void {
+    for (let controlName in this.settingsForm.controls) {
+      this.delegateSettingsToFunctions(controlName, this.settingsForm.controls[controlName]);
+    }
+  }
+
+  delegateSettingsToFunctions(settingName, setting: object): void {
+    const settingsFunctionDelegator: Object = {
+      "alertPreferences": (setting) => { this.saveAlertPreferences(setting) },
+      "volumePreferences": (setting) => { this.saveVolumePreferences(setting) },
+      "notificationPreferences": (setting) => { this.saveNotificationPreferencers(setting) },
+      "timerPreferences": (setting) => { this.saveTimerPreferences(setting) }
+    };
+
+    //Delegate the settingchanges to their respective functions using the
+    // settingsFunctionDelegator object
+    for (const [_settingName, callback] of Object.entries(settingsFunctionDelegator)) {
+      if (String(settingName) == _settingName) { callback(setting); }
+    }
+  }
+
+  saveAlertPreferences(settingChanges: Object): void {
+    console.log(settingChanges);
+  }
+
+  saveNotificationPreferencers(settingChanges: Object): void {
+    console.log(settingChanges);
+  }
+
+  saveTimerPreferences(settingChanges: Object): void {
+    console.log(settingChanges);
+  }
+
+  saveVolumePreferences(settingChanges: Object): void {
+    console.log(settingChanges);
   }
 }
